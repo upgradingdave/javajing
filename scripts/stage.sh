e4d1d665e6271b0dabc535e53f7d78e090621b10
Dave Paroulek <dparoulek@gmail.com>
2012-06-29 21:14:51 -0400

    Publish episode5 unit test with spring

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -8,4 +8,5 @@ cp "ruby-from-java/show-notes.org" "$POSTSDIR/2012-06-01-ruby-from-java.org"
 cp "slf4j/show-notes.org" "$POSTSDIR/2012-06-08-slf4j.org"
 cp "maven-repos/show-notes.org" "$POSTSDIR/2012-06-15-maven-repo.org"
 cp "artifactory/show-notes.org" "$POSTSDIR/2012-06-22-artifactory.org"
+cp "unit-test-with-spring/show-notes.org" "$POSTSDIR/2012-06-29-unit-test-with-spring.org"
 
