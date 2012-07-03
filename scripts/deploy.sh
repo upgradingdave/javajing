48e0380e6cf0997ac903b34b6203501623326f7d
Dave Paroulek <dparoulek@gmail.com>
2012-07-03 11:05:01 -0400

    Submit sitemap to google and bing
    - add publish.sh for running jekyll from root dir
    - update deploy.sh to use rsync for faster deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,10 @@
 
 WEBAPPDIR="/Users/dparoulek/projects/javajing/site/src/main/webapp"
 SITEDIR="$WEBAPPDIR/jekyll/_site"
+PORTNUMBER=31000
 
-scp -P31000 -r $SITEDIR/* dparoulek@mercury:~/apps/javajing/prod
+rsync -avz -e "ssh -p $PORTNUMBER" $SITEDIR/* dparoulek@mercury:~/apps/javajing/prod
+#rsync -vac $SITEDIR/* dparoulek@mercury:~/apps/javajing/prod
+#scp -P31000 -r $SITEDIR/* 
 
 
---
+++ scripts/publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bash -c "cd site/src/main/webapp/jekyll; jekyll"
+
