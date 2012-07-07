920597d9ecd8c995900ecba77eecec8cf1b34de1
Dave Paroulek <dparoulek@gmail.com>
2012-07-07 14:33:32 -0400

    Publish episode6

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -9,4 +9,5 @@ cp "slf4j/show-notes.org" "$POSTSDIR/2012-06-08-slf4j.org"
 cp "maven-repos/show-notes.org" "$POSTSDIR/2012-06-15-maven-repo.org"
 cp "artifactory/show-notes.org" "$POSTSDIR/2012-06-22-artifactory.org"
 cp "unit-test-with-spring/show-notes.org" "$POSTSDIR/2012-06-29-unit-test-with-spring.org"
+cp "apache-proxy/show-notes.org" "$POSTSDIR/2012-07-06-apache-proxy.org"
 
